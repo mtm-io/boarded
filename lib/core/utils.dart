df62014f7411e5f2ccfa6105488740ab6359a8aa
mishaqq <78998611+mishaqq@users.noreply.github.com>
2023-08-16 08:21:47 +0200

    Revert "Backup Update 16.08.23"

--- lib/core/utils.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnackBar(BuildContext context, String text) {
-  ScaffoldMessenger.of(context)
-    ..hideCurrentSnackBar()
-    ..showSnackBar(SnackBar(content: Text(text)));
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,22 @@
-import 'package:boarded/core/common/loader.dart';
-import 'package:boarded/router.dart';
-import 'package:boarded/tabs/auth/controller/auth_controller.dart';
-import 'package:boarded/utils/pallete.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:routemaster/routemaster.dart';
-import 'core/common/error_text.dart';
-import 'firebase_options.dart';
-import 'models/user_model.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const ProviderScope(
-    child: MyApp(),
-  ));
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends ConsumerStatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
-}
-
-class _MyAppState extends ConsumerState<MyApp> {
-  UserModel? userModel;
-  // Get current user and save it's state to userModel.
-  void getData(WidgetRef ref, User data) async {
-    userModel = await ref.watch(authControllerProvider.notifier).getUserData(data.uid).first;
-    ref.read(userProvider.notifier).update((state) => userModel);
-    setState(() {});
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ref.watch(authStateChangeProvider).when(
-          data: (data) => ScreenUtilInit(
-            designSize: const Size(390, 844),
-            builder: (context, child) => MaterialApp.router(
-              debugShowCheckedModeBanner: false,
-              title: 'Boardify',
-              theme: Pallete.darkTheme,
-              //theme: ref.watch(themeNotifierProvider),
-              routerDelegate: RoutemasterDelegate(
-                routesBuilder: (context) {
-                  if (data != null) {
-                    getData(ref, data);
-                    if (userModel != null) {
-                      return loggedInRoute;
-                    }
-                  }
-                  return loggedOutRoute;
-                },
-              ),
-              routeInformationParser: const RoutemasterParser(),
-            ),
-          ),
-          error: (error, stackTrace) => ErrorText(error: error.toString()),
-          loading: () => const Loader(),
-        );
+    return const MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        body: ColoredBox(
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
