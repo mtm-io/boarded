9b8b4b7731a3cef8023abfcf005e05b6d2efbfef
mishaqq <dron7432@gmail.com>
2023-08-16 15:35:08 +0200

    AuthCards rotation animations Pallete adjustments like: iconColor and imageColor AuthAppleCard AND AuthGoogleCard new reusable Widgets

--- lib/tabs/rooms/repository/room_repository.dart
+++ lib/tabs/rooms/repository/room_repository.dart
@@ -2,7 +2,6 @@ import 'package:boarded/core/constants/firebase_constants.dart';
 import 'package:boarded/core/failure.dart';
 import 'package:boarded/core/providers/firebase_providers.dart';
 import 'package:boarded/core/type_defs.dart';
-import 'package:boarded/models/board_games_model.dart';
 import 'package:boarded/models/room_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,7 +13,8 @@ final roomRepositoryProvider = Provider((ref) {
 
 class RoomRepository {
   final FirebaseFirestore _firestore;
-  RoomRepository({required FirebaseFirestore firestore}) : _firestore = firestore;
+  RoomRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
 
   FutureVoid createRoom(Room room) async {
     try {
@@ -37,10 +37,13 @@ class RoomRepository {
     }
   }
 
-  CollectionReference get _rooms => _firestore.collection(FirebaseConstants.roomsCollection);
-  CollectionReference get _games => _firestore.collection(FirebaseConstants.gamesCollection);
+  CollectionReference get _rooms =>
+      _firestore.collection(FirebaseConstants.roomsCollection);
+  CollectionReference get _games =>
+      _firestore.collection(FirebaseConstants.gamesCollection);
 
   Stream<QuerySnapshot> getBoardGames() {
-    return _games.snapshots(); //.map((event) => BoardGames.fromMap(event.data() as Map<String, dynamic>));
+    return _games
+        .snapshots(); //.map((event) => BoardGames.fromMap(event.data() as Map<String, dynamic>));
   }
 }
--- lib/utils/pallete.dart
+++ lib/utils/pallete.dart
@@ -3,44 +3,54 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Pallete {
   // Colors
-  static const blackColor = Color.fromRGBO(0, 0, 0, 1); // primary color
-  static const greyColor = Color.fromRGBO(26, 39, 45, 1); // secondary color
+  static const blackColor = Color.fromRGBO(0, 0, 0, 1);
+  static const appBarColor = Color.fromRGBO(0, 0, 0, 1);
+  static const greyColor = Color.fromRGBO(13, 31, 35, 1);
   static const drawerColor = Color.fromRGBO(18, 18, 18, 1);
   static const whiteColor = Colors.white;
-  static var redColor = Colors.red.shade500;
+  static const redColor = Colors.red;
   static var blueColor = Colors.blue.shade300;
-  static var loaderColor = const Color.fromRGBO(234, 234, 234, 1);
-
+  static const loaderColor = Colors.white;
+  static const imageColor = Colors.white;
+  static const iconColor = Colors.white;
+  static const textColor = Colors.white;
   static ThemeData darkTheme = ThemeData(
     primaryColor: Pallete.greyColor,
     scaffoldBackgroundColor: Pallete.blackColor,
 
     iconTheme: const IconThemeData(
-      color: whiteColor,
+      color: iconColor,
     ),
     appBarTheme: const AppBarTheme(
       centerTitle: true,
-      backgroundColor: blackColor,
+      backgroundColor: appBarColor,
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: blackColor,
-        side: const BorderSide(
-          width: 2.0,
-          color: whiteColor,
-        ),
+        side: const BorderSide(width: 2.0, color: whiteColor),
       ),
     ),
 
     // font
     fontFamily: 'SpaceGrotesk',
+
     //text style
     textTheme: TextTheme(
-      bodyMedium: TextStyle(
-          fontSize: 23.sp,
-          color: Colors.white,
-          fontFamily: 'SpaceGrotesk',
-          fontWeight: FontWeight.w400),
-    ),
+        bodyLarge: TextStyle(
+            fontSize: 23.sp,
+            color: textColor,
+            fontFamily: 'SpaceGrotesk',
+            fontWeight: FontWeight.w400),
+        bodyMedium: TextStyle(
+            fontSize: 17.sp,
+            color: textColor,
+            fontFamily: 'SpaceGrotesk',
+            fontWeight: FontWeight.w400),
+        bodySmall: TextStyle(
+            fontSize: 13.sp,
+            color: textColor,
+            fontFamily: 'SpaceGrotesk',
+            fontWeight: FontWeight.w400)),
   );
 }
