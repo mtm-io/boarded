91f729d6b84a2e45423558a43b2211fd66514e77
mishaqq <dron7432@gmail.com>
2023-08-29 20:47:28 +0200

    Home Search; BottomNavBar Anim + New Icons; Cards

--- lib/bottom_nav.dart
+++ lib/bottom_nav.dart
@@ -1,10 +1,13 @@
+import 'package:boarded/core/constants/fade_indexed_stack.dart';
 import 'package:boarded/tabs/home/screens/home_screen.dart';
 import 'package:boarded/tabs/host/screens/host_page.dart';
 import 'package:boarded/tabs/profile/screens/profile.dart';
+import 'package:boarded/utils/pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
-
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
+import 'package:animations/animations.dart';
 import 'core/constants/constants.dart';
 
 class BottomNavigator extends StatefulWidget {
@@ -26,66 +29,86 @@ class _BottomNavigatorState extends State<BottomNavigator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
+      body: AnimatedIndexedStack(
         index: _selectedIndex,
         children: _widgetOptions,
       ),
       bottomNavigationBar: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(bottom: 5.h),
-          child: Theme(
-            data: ThemeData(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
+        child: Theme(
+          data: ThemeData(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              border: const GradientBoxBorder(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white,
+                    Pallete.blackColor,
+                    Pallete.blackColor,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                width: 1.2,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.r),
+                topRight: Radius.circular(15.r),
+              ),
             ),
-            child: BottomNavigationBar(
-              //unselectedItemColor: Color.fromRGBO(126, 123, 123, 1),
-              //selectedItemColor: Colors.white,
-              enableFeedback: false,
-              backgroundColor: const Color.fromARGB(255, 12, 12,
-                  12), //changes background of Botton Navigation Bar
-              showSelectedLabels: true,
-              showUnselectedLabels: false,
-              selectedLabelStyle: const TextStyle(fontSize: 15),
-              items: [
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    Constants.diceD20,
-                    height: 45.sp,
-                    width: 45.sp,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20.h, top: 3.h),
+              child: BottomNavigationBar(
+                elevation: 0,
+                //unselectedItemColor: Color.fromRGBO(126, 123, 123, 1),
+                //selectedItemColor: Colors.white,
+                enableFeedback: false,
+                backgroundColor: Colors.transparent,
+                //changes background of Botton Navigation Bar
+                showSelectedLabels: true,
+                showUnselectedLabels: false,
+                selectedLabelStyle: const TextStyle(fontSize: 15),
+                items: [
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      Constants.host,
+                      height: 25.sp,
+                      width: 25.sp,
+                      colorFilter:
+                          const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                    ),
+                    label: "",
                   ),
-                  label: "",
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    Constants.compas,
-                    height: 45.sp,
-                    width: 40.sp,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      Constants.home,
+                      height: 25.sp,
+                      width: 25.sp,
+                      colorFilter:
+                          const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                    ),
+                    label: "",
                   ),
-                  label: "",
-                  backgroundColor: Colors.red,
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    Constants.avatar,
-                    height: 45.sp,
-                    width: 45.sp,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                  ),
-                  label: "",
-                )
-              ],
-              currentIndex: _selectedIndex,
-              onTap: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      Constants.profileV2,
+                      height: 25.sp,
+                      width: 25.sp,
+                      colorFilter:
+                          const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                    ),
+                    label: "",
+                  )
+                ],
+                currentIndex: _selectedIndex,
+                onTap: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+              ),
             ),
           ),
         ),
--- lib/tabs/home/screens/home_screen.dart
+++ lib/tabs/home/screens/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:boarded/tabs/home/tamplates/room_card.dart';
+import 'package:boarded/utils/pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
+
+import '../../../core/common/anim_searchbar.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -11,42 +16,153 @@ class HomePage extends ConsumerStatefulWidget {
 
 class _HomePageState extends ConsumerState<HomePage> {
   List<String> randomImages = [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaOjCZSoaBhZyODYeQMDCOTICHfz_tia5ay8I_k3k&s'
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaOjCZSoaBhZyODYeQMDCOTICHfz_tia5ay8I_k3k&s',
+    'https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg',
+    'https://pixlr.com/images/index/remove-bg.webp',
+    'https://images.pexels.com/photos/4154191/pexels-photo-4154191.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   ];
+  TextEditingController textController = TextEditingController();
+  bool _isOpen = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("Home Page")),
-        body: ListView(
-          children: [
-            const RoomCard(
-              time: "Tomorrow, 7:00 - 22:00",
-              title: 'Переможнi настолки у парку "Муромець"',
-              place: "Kyiv, Ukraine",
-              tags: [
-                "Just Chatting",
-                "Exploding Kittens",
-                "Alias",
-              ],
+    return SafeArea(
+      child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(
+                MediaQuery.of(context).size.height / 10), // Set this height
+            child: Container(
+              decoration: BoxDecoration(
+                border: const GradientBoxBorder(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.white,
+                      Pallete.blackColor,
+                      Pallete.blackColor,
+                      Pallete.blackColor,
+                      Pallete.blackColor,
+                      Pallete.blackColor,
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                  width: 0.5,
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15.r),
+                  bottomRight: Radius.circular(15.r),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 13.w),
+                        child: _isOpen
+                            ? SizedBox()
+                            : Text(
+                                "Search",
+                                style: TextStyle(
+                                    fontSize: 26.sp,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: EdgeInsets.only(right: 15.w),
+                        child: SafeArea(
+                          child: ClipRect(
+                            child: AnimSearchBar(
+                              height: 45.h,
+                              searchBarOpen: (isOpen) {
+                                Future.delayed(const Duration(milliseconds: 80),
+                                    () {
+                                  setState(() {
+                                    if (isOpen == 1) {
+                                      _isOpen = true;
+                                    } else {
+                                      _isOpen = false;
+                                    }
+                                  });
+                                });
+                              },
+                              width: MediaQuery.of(context).size.width - 30.w,
+                              textController: textController,
+                              onSuffixTap: () {
+                                //setState(() {
+                                // textController.clear();
+                                // });
+                              },
+                              onSubmitted: (text) {
+                                print(text);
+                              },
+                              autoFocus: false,
+                              style: TextStyle(
+                                  fontSize: 17.sp, color: Pallete.whiteColor),
+                              textFieldColor: Pallete.blackColor,
+                              searchIconColor: Pallete.whiteColor,
+                              color: Pallete.blackColor,
+                              textFieldIconColor: Pallete.whiteColor,
+                              rtl: true,
+                            ),
+                          ),
+                        ),
+                      ),
+                      //  Padding(
+                      //    padding:  EdgeInsets.only(right: 10.sp),
+                      //    child: Icon(
+                      //      Icons.search,
+                      //      size: 30.sp,
+                      //    ),
+                      // ),
+                    ],
+                  )
+                ],
+              ),
             ),
-            RoomCard(
-              images: randomImages,
-              time: "Friday, 15:00 - 18:00",
-              title: "Просто Настолки",
-              place: "Kyiv, Ukraine",
-              tags: [
-                "Exploding Kittens",
-                "Just Chatting",
-                "Alias",
-              ],
-            ),
-            RoomCard(
-              images: randomImages,
-              time: "Friday, 15:00 - 18:00",
-              title: "Fuck You",
-              place: "Kyiv, Ukraine",
-            ),
-          ],
-        ));
+          ),
+          body: ListView(
+            children: [
+              RoomCard(
+                time: "Tomorrow, 7:00 - 22:00",
+                title: 'Board Games English Club',
+                place: "Kyiv, Ukraine",
+                tags: [
+                  "Just Chatting",
+                  "Exploding Kittens",
+                  "Alias",
+                ],
+              ),
+              RoomCard(
+                images: randomImages,
+                time: "Friday, 15:00 - 18:00",
+                title: "Просто Настолки",
+                place: "Mariupol, Ukraine",
+                tags: [
+                  "Exploding Kittens",
+                  "Just Chatting",
+                  "Alias",
+                ],
+              ),
+              RoomCard(
+                images: randomImages,
+                time: "Friday, 15:00 - 18:00",
+                title: "Настолки на набережній",
+                place: "Obolonska Naberezhna Street, 20, Kyiv, Ukraine",
+                tags: [
+                  "Exploding Kittens",
+                  "Just Chatting",
+                  "Mafia",
+                  "Alias",
+                  "Jaust Chilling",
+                  "Soap",
+                ],
+              ),
+            ],
+          )),
+    );
   }
 }
